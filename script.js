091350b2b5910cae453444789c574b6ddf365abd
AzamAnsari8 <ansari08azam@gmail.com>
2024-10-30 00:24:40 +0530

    complete

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let date = document.getElementById("date");
 let month = document.getElementById("month");
 let year = document.getElementById("year");
 
-
 let hours = document.getElementById("hours");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
@@ -11,17 +10,11 @@ let period = document.getElementById("period");
 
 setInterval(() => {
     let currentTime = new Date();
-    
+
+    date.innerHTML = currentTime.getDate();
+    month.innerHTML = currentTime.getUTCMonth() + 1;
+    year.innerHTML = currentTime.getFullYear();
     hours.innerHTML = currentTime.getHours();
     min.innerHTML = currentTime.getMinutes();
     sec.innerHTML = currentTime.getSeconds();
-}, 1000)
-
-
-setInterval(() => {
-    let currentTime = new Date();
-    
-    date.innerHTML = currentTime.getDate(); 
-    month.innerHTML = currentTime.getUTCMonth() +1;
-    year.innerHTML = currentTime.getFullYear();
 }, 1000)
